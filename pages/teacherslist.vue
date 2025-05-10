754ac42669299d19aebd93d5270b4ac53753de45
Andrea <andrea1.sacconi@mail.polimi.it>
2025-05-10 19:09:24 +0200

    Aggiunta navbar, footer e titolo in teacherlist

--- pages/teacherslist.vue
+++ pages/teacherslist.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="teachers-list">
-    <ItemTeacher
-        v-for="teacher in teachers"
-        :key="teacher.id"
-        :title="`${teacher.name} ${teacher.surname}`"
-        :imageUrl="teacher.image[0]?.url"
-        :route="`/teacher/${teacher.id}`"
-    />
+  <div class="page-container">
+    <header>
+      <div  class="navbar">
+        <Navbar/>
+      </div>
+    </header>
+    <body>
+      <h1>Teachers</h1>
+      <div class="teachers-list">
+        <ItemTeacher
+            v-for="teacher in teachers"
+            :key="teacher.id"
+            :title="`${teacher.name} ${teacher.surname}`"
+            :imageUrl="teacher.image[0]?.url"
+            :route="`/teacher/${teacher.id}`"
+        />
+      </div>
+    </body>
+    <footer>
+      <div>
+        <Footer />
+      </div>
+    </footer>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
 import ItemTeacher from '@/components/ItemTeacher.vue';
+import Navbar from "~/components/navbar.vue";
 
 const teachers = ref([]);
 
@@ -23,6 +39,18 @@ onMounted(async () => {
 </script>
 
 <style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+}
+
+.navbar {
+  /*position: fixed;*/
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000; /* Assicura che la navbar sia sopra gli altri elementi */
+}
 .teachers-list {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
