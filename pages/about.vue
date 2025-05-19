dd8ace366c7e5607ad35247b594e1d71b6c78a8b
Davide <davide.rutigliano@yahoo.it>
2025-05-19 16:01:12 +0200

    Update of About Us page

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,4 @@
 <script>
-
-import {defineComponent} from "vue";
-import ContactComponent from "~/components/ContactComponent.vue";
-import FAQ from "~/components/FAQ.vue";
-
-export default defineComponent({
-  components: {FAQ, ContactComponent}
-})
 </script>
 
 <template>
@@ -15,44 +7,39 @@ export default defineComponent({
       <Navbar />
     </div>
     <div class="main">
-      <div class="error-container">
         <div class="content">
 
-          <br><h1>Contacts of our team</h1>
-          <h2>Even websites lose their balance sometimes.</h2>
+          <br><h1>About Us</h1>
+          <img src="/yogaCenterAboutUs.jpg" alt="About Us Image" id="aboutUs"/>
 
         </div>
-        <img src="/contact.png" alt="Contact Image" class="error-image"/>
-      </div>
-    </div>
-    <div class="contact-row">
-      <ContactComponent
-          v-for="(contact, index) in contacts"
-          :key="index"
-          :title="contact.title"
-          :imageUrl="contact.imageUrl"
-      />
-    </div>
 
-    <h2>Dove siamo</h2>
-    <div class="mappa-container">
+      <div class="ourHistoryParent">
+        <b class="ourHistory">Our History</b>
+        <div class="loremIpsumDolor">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </div>
+        <div class="loremIpsumDolor1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </div>
+      </div>
 
-      <div class="address">
-        <p>Address: Piazza Leonardo da Vinci, 32, 20133 Milano MI</p>
+      <div class="rectangleParent">
+        <img class="frameChild" alt="" src="/aboutUsTeacher1.png" />
+        <div class="teacher">
+          <img class="imageIcon" alt="" src="/aboutUsTeacher2.png" />
+        </div>
       </div>
-      <div class="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5595.199970422994!2d9.227746426630475!3d45.47786138252662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786c6f67dd11753%3A0x4fa6e0b212f7dd0c!2sPiazza%20Leonardo%20da%20Vinci%2C%2032%2C%2020133%20Milano%20MI!5e0!3m2!1sit!2sit!4v1747504026638!5m2!1sit!2sit"
-                width="600"
-                height="450"
-                style="border:0;"
-                allowfullscreen=""
-                loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
+
+
+
+      <div class="AboutUsEpilogueContainer">
+        <h3>Regulation</h3>
+        <ol>
+          <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</li>
+          <li>Lorem ipsum dolor sit amet</li>
+          <li>sed do eiusmod tempor incididunt ut labore et dolore m</li>
+        </ol>
       </div>
+
     </div>
 
-    <FAQ/>
     <div class="footer">
       <Footer />
     </div>
@@ -62,5 +49,110 @@ export default defineComponent({
 
 <style scoped>
 @import "../assets/global.css" ;
+
+#aboutUs{
+  margin-top: 80px;
+}
+
+.ourHistory {
+  width: 857px;
+  position: relative;
+  font-size: 60px;
+  line-height: 114px;
+  display: inline-block;
+  text-align: center;
+  flex-shrink: 0;
+}
+.loremIpsumDolor {
+  width: 560px;
+  position: relative;
+  line-height: 41px;
+  font-weight: 300;
+  display: inline-block;
+  height: 492px;
+  flex-shrink: 0;
+}
+.loremIpsumDolor1 {
+  width: 560px;
+  position: relative;
+  line-height: 41px;
+  font-weight: 300;
+  display: inline-block;
+  height: 500px;
+  flex-shrink: 0;
+}
+.ourHistoryParent {
+  position: relative;
+  top: 0px;
+  left: calc(50% - 616px);
+  width: 1224px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-content: center;
+  gap: 38px 104px;
+}
+
+.frameChild {
+  width: 616px;
+  position: relative;
+  height: 580px;
+  object-fit: cover;
+}
+.imageIcon {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  top: 0%;
+  right: 0%;
+  bottom: 0%;
+  left: 0%;
+  max-width: 100%;
+  overflow: hidden;
+  max-height: 100%;
+  object-fit: cover;
+}
+.teacher {
+  width: 612px;
+  position: relative;
+  height: 543px;
+}
+.rectangleParent {
+  position: relative;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 68px;
+  margin-left: 400px;
+}
+
+ol {
+  margin: 0;
+  font-family: inherit;
+  font-size: inherit;
+  padding-left: 43px;
+}
+
+li {
+  margin-bottom: 50px;
+}
+
+.AboutUsEpilogueContainer {
+  width: 1430px;
+  position: relative;
+  font-size: 32px;
+  line-height: 41px;
+  font-weight: 300;
+  font-family: 'Nunito Sans';
+  color: #000;
+  text-align: justify;
+  display: inline-block;
+  height: 587px;
+  margin-top: 160px;
+}
 
 </style>
