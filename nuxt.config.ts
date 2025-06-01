b721b1718dc945d54124b9a25b4a80220ca9186f
DaveRuti <davide1.rutigliano@mail.polimi.it>
2025-06-01 02:25:35 +0200

    Commit for GitHub Pages correct loading

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['@/assets/css/global.css']
+  css: ['@/assets/css/global.css'],
+  app: {
+    baseURL: '/HYP_YogaCenter/'
+  }
 })
